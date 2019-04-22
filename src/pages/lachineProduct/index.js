e0561cda9f86c02b0bce4159643135bac3fef870
wangkuan1994 <1913992554@qq.com>
2019-04-22 14:11:07 +0800

    拉新产品接口调整

--- src/pages/lachineProduct/index.js
+++ src/pages/lachineProduct/index.js
@@ -1,46 +1,56 @@
 import React, { Component } from 'react';
 import "./index.css";
 import { Tabs, NavBar, Icon } from 'antd-mobile'
+import { lachineList, recommendList } from '../api/lachineProduct'
 
 class LachineProduct extends Component {
     constructor(...args) {
         super(...args)
 
         this.state = {
-            list_data: []
+            Lachine_list: [], // 拉新列表数据
+            recommend_list: [], // 产品列表数据
+            initialPage: 0,
         }
     }
 
+    // 获取拉新列表
+    async getLachineList() {
+        let Lachine_list = await lachineList({
+            "pageNo": 1,
+            "pageSize": 10,
+        })
+        let { result } = Lachine_list
+        this.setState({
+            Lachine_list: result || [],
+        })
+    }
+
+    // 获取产品推荐列表数据
+    async getRecommendList() {
+        let recommend_list = await recommendList({
+            "pageNo": 1,
+            "pageSize": 1,
+        })
+        console.log(recommend_list)
+    }
+
     componentDidMount() {
+        try {
+            this.getLachineList()
+            this.getRecommendList()
+        } catch (e) {
+            console.log(e)
+        }
+        // tab栏标题
         const tabs = [
             { title: '拉新会员' },
             { title: '产品推荐' },
         ];
-        let list_data = [
-            {
-                id: 1,
-                title: '马尔代夫卡尼岛Club Med度假村高级会所2晚豪华水屋，送往返机票1',
-                price: 6600,
-                price2: 200
-            },
-            {
-                id: 2,
-                title: '马尔代夫卡尼岛Club Med度假村高级会所2晚豪华水屋，送往返机票2',
-                price: 3333,
-                price2: 200
-            }
-        ]
-        this.setState({
-            list_data,
-            tabs
-        })
-    }
 
-    fnGoto() {
-        
-        this.props.history.push(
-            '/'
-        )
+        this.setState({
+            tabs,
+        })
     }
 
     render() {
@@ -52,23 +62,25 @@ class LachineProduct extends Component {
                     onLeftClick={() => window.history.go(-1)}
                 ></NavBar>
                 <div className="lachineProduct ">
-                    <Tabs tabs={this.state.tabs} initialPage={0} tabBarActiveTextColor={'#000000'} tabBarInactiveTextColor={'#999999'} tabBarUnderlineStyle={{ border: '0.5px #ffc147 solid' }} animated={true} useOnPan={true}>
+                    <Tabs tabs={this.state.tabs} initialPage={this.state.initialPage} tabBarActiveTextColor={'#000000'} tabBarInactiveTextColor={'#999999'} tabBarUnderlineStyle={{ border: '0.5px #ffc147 solid' }} animated={true} useOnPan={true}>
                         <div>
                             <section className="lachine" >
                                 <ul>
-                                    <li>
-                                        <div className="lachine-top clearfix">
-                                            <div className="picture"><img src="./imgs/header-portrait.jpg" alt="" /></div>
-                                            <div className="characters">
-                                                <p className="characters-telephone">18888888888</p>
-                                                <p className="characters-time">
-                                                    <span>推荐时间</span>
-                                                    <span>2018-12-12</span>
-                                                    <span>09:00</span>
-                                                </p>
+                                    {this.state.Lachine_list.map(item => (
+                                        <li key={item.recommendId}>
+                                            <div className="lachine-top clearfix">
+                                                <div className="picture"><img src="./imgs/header-portrait.jpg" alt="" /></div>
+                                                <div className="characters">
+                                                    <p className="characters-telephone">{item.mobile}</p>
+                                                    <p className="characters-time">
+                                                        <span>推荐时间</span>
+                                                        <span>{item.recommendDate}</span>
+                                                        <span></span>
+                                                    </p>
+                                                </div>
                                             </div>
-                                        </div>
-                                    </li>
+                                        </li>
+                                    ))}
                                 </ul>
                             </section>
                         </div>
