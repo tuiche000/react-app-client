ca1d1f52974cd36807f87151fda3aa2edb87db26
wangkuan1994 <1913992554@qq.com>
2019-04-30 15:46:51 +0800

    修改高度

--- src/pages/StarProducts/index.js
+++ src/pages/StarProducts/index.js
@@ -195,7 +195,7 @@ class StarProducts extends Component {
                     damping={100}
                     ref={el => this.ptr = el}
                     style={{
-                        minHeight: this.state.height,
+                        height: this.state.height,
                         overflow: 'auto',
                     }}
                     direction="up"
